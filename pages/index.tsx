0c2ce2faef7d497b6d0765d3b466d81f50958ea6
1juanherrera <juansebas751@gmail.com>
2023-04-19 16:44:17 -0500

    changes by owner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-red-200">HOME, HELLO WORLD</div>
+      <div className="bg-red-200">HOME, HELLO WORLD!!!</div>
     </>
   );
 }
